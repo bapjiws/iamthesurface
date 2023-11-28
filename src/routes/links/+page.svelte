a86ea2fd63a87dfbe7cc798eecc251bef35c558d
Alexey Boklin <alex.boklin@gmail.com>
2023-11-28 17:40:14 +0100

    Load images

--- src/routes/links/+page.svelte
+++ src/routes/links/+page.svelte
@@ -1,3 +1,8 @@
+<script>
+	import flowers1 from '$lib/images/flowers_1.jpg';
+	import flowers2 from '$lib/images/flowers_2.jpg';
+</script>
+
 <svelte:head>
 	<title>About</title>
 	<meta name="description" content="About this app" />
@@ -5,6 +10,10 @@
 
 <div class="text-column">
 	<h1>Links</h1>
+
+	<img src={flowers1} alt="Flowers image 1" />
+
+	<img src={flowers2} alt="Flowers image 2" />
 
 	<p>Social links:</p>
 </div>
