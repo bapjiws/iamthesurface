e8ef50b78e8c3375fb1f44fa01b65df04762d230
Alexey Boklin <alex.boklin@gmail.com>
2023-11-28 21:50:59 +0100

    Video background WIP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	/* import Counter from './Counter.svelte'; */
+	/* import welcome from '$lib/images/svelte-welcome.webp'; */
+	/* import welcome_fallback from '$lib/images/svelte-welcome.png'; */
+
+  import video from '$lib/videos/1080.mp4';
 </script>
 
 <svelte:head>
@@ -9,51 +11,61 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+<video id="background-video" autoplay loop muted>
+  <source src={video} type="video/mp4">
+</video>
 
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
+<!-- <section> -->
+<!-- 	<h1> -->
+<!-- 		<span class="welcome"> -->
+<!-- 			<picture> -->
+<!-- 				<source srcset={welcome} type="image/webp" /> -->
+<!-- 				<img src={welcome_fallback} alt="Welcome" /> -->
+<!-- 			</picture> -->
+<!-- 		</span> -->
+<!---->
+<!-- 		to your new<br />SvelteKit app -->
+<!-- 	</h1> -->
+<!---->
+<!-- 	<h2> -->
+<!-- 		try editing <strong>src/routes/+page.svelte</strong> -->
+<!-- 	</h2> -->
+<!---->
+<!-- 	<Counter /> -->
+<!-- </section> -->
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
+  video {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 
-	h1 {
-		width: 100%;
-	}
+	/* section { */
+	/* 	display: flex; */
+	/* 	flex-direction: column; */
+	/* 	justify-content: center; */
+	/* 	align-items: center; */
+	/* 	flex: 0.6; */
+	/* } */
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
+	/* h1 { */
+	/* 	width: 100%; */
+	/* } */
 
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+	/* .welcome { */
+	/* 	display: block; */
+	/* 	position: relative; */
+	/* 	width: 100%; */
+	/* 	height: 0; */
+	/* 	padding: 0 0 calc(100% * 495 / 2048) 0; */
+	/* } */
+
+	/* .welcome img { */
+	/* 	position: absolute; */
+	/* 	width: 100%; */
+	/* 	height: 100%; */
+	/* 	top: 0; */
+	/* 	display: block; */
+	/* } */
 </style>
