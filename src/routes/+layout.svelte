23290aeac14f1188161305dcc4ab392aa2197352
Alexey Boklin <alex.boklin@gmail.com>
2023-11-29 12:29:05 +0100

    Set up icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,15 @@
 	import { browser } from '$app/environment';
 	import { page } from '$app/stores';
 	import { webVitals } from '$lib/vitals';
-	import Header from './Header.svelte';
+	/* import Header from './Header.svelte'; */
 	import './styles.css';
+
+	import Bandcamp from '$lib/icons/Bandcamp.svelte';
+	import Instagram from '$lib/icons/Instagram.svelte';
+	import SoundCloud from '$lib/icons/SoundCloud.svelte';
+	import Spotify from '$lib/icons/Spotify.svelte';
+	import TikTok from '$lib/icons/TikTok.svelte';
+	import YouTube from '$lib/icons/YouTube.svelte';
 
 	/** @type {import('./$types').LayoutServerData} */
 	export let data;
@@ -25,7 +32,48 @@
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		<a
+			href="https://bapjiws.bandcamp.com"
+			target="_blank"
+			rel="noopener"
+		>
+			<Bandcamp />
+		</a>
+		<a
+			href="https://soundcloud.com/iamthesurface_iamthelion"
+			target="_blank"
+			rel="noopener"
+		>
+			<SoundCloud />
+		</a>
+		<a
+			href="https://www.youtube.com/@iamthesurface_iamthelion"
+			target="_blank"
+			rel="noopener"
+		>
+			<YouTube />
+		</a>
+		<a
+			href="https://www.instagram.com/iamthesurface_official/"
+			target="_blank"
+			rel="noopener"
+		>
+			<Instagram />
+		</a>
+		<a
+			href="https://www.tiktok.com/@iamthesurface_official"
+			target="_blank"
+			rel="noopener"
+		>
+			<TikTok />
+		</a>
+		<a
+			href="https://open.spotify.com/track/6kYQstzc8ptJIIINlm6QSD"
+			target="_blank"
+			rel="noopener"
+		>
+			<Spotify />
+		</a>
 	</footer>
 </div>
 
@@ -49,19 +97,22 @@
 
 	footer {
 		display: flex;
-		flex-direction: column;
+		/* flex-direction: column; */
 		justify-content: center;
 		align-items: center;
-		padding: 12px;
+		padding: 24px;
 	}
 
 	footer a {
 		font-weight: bold;
+    margin: 8px;
+		height: 32px;
+		width: 32px;
 	}
 
 	@media (min-width: 480px) {
 		footer {
-			padding: 12px 0;
+			padding: 24px 0;
 		}
 	}
 </style>
--- src/routes/links/+page.svelte
+++ src/routes/links/+page.svelte
@@ -1,8 +1,6 @@
 <script>
 	import flowers1 from '$lib/images/flowers_1.jpg';
 	import flowers2 from '$lib/images/flowers_2.jpg';
-
-	import Tidal from '$lib/images/Tidal.svelte';
 </script>
 
 <svelte:head>
@@ -18,11 +16,4 @@
 	<!-- <img src={flowers2} alt="Flowers image 2" /> -->
 
 	<!-- TODO: rel="me nofollow ugc"  -->
-	<a
-		href="https://tidal.com/browse/track/326087341"
-		target="_blank"
-		rel="noopener noreferrer"
-	>
-		<Tidal />
-	</a>
 </div>
