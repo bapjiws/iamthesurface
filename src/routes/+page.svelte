50785b988ab38fc6ac1b98dd77ecb8bf49e77583
Alexey Boklin <alex.boklin@gmail.com>
2024-08-29 19:46:19 +0200

    Delete content 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,10 +10,6 @@
 </svelte:head>
 
 <article>
-	Like mountains that ceased the above,<br />
-	Like autumn reclaiming its leaves,<br />
-	Untake from the ones that you love,<br />
-	Ungive from your most precious deeds.
 </article>
 
 <!-- <section> -->
@@ -35,32 +31,4 @@
 <!-- 	<Counter /> -->
 <!-- </section> -->
 
-<style>
-	/* section { */
-	/* 	display: flex; */
-	/* 	flex-direction: column; */
-	/* 	justify-content: center; */
-	/* 	align-items: center; */
-	/* 	flex: 0.6; */
-	/* } */
-
-	/* h1 { */
-	/* 	width: 100%; */
-	/* } */
-
-	/* .welcome { */
-	/* 	display: block; */
-	/* 	position: relative; */
-	/* 	width: 100%; */
-	/* 	height: 0; */
-	/* 	padding: 0 0 calc(100% * 495 / 2048) 0; */
-	/* } */
-
-	/* .welcome img { */
-	/* 	position: absolute; */
-	/* 	width: 100%; */
-	/* 	height: 100%; */
-	/* 	top: 0; */
-	/* 	display: block; */
-	/* } */
-</style>
+<style></style>
